4779d8fe612c1f6e631e2ca758119ccaac8fece3
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-09-04 16:21:30 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,7 +17,7 @@ class Config(object):
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 5852645041)
+	OWNER_ID = int(os.environ.get("OWNER_ID", 5852645041))
 	CAPTION = "By @AHToolsBot"
 	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
 	DATABASE_URL = os.environ.get("DATABASE_URL")
