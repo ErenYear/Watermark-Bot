00af809438556cc2ed2460369977b1390e2702c7
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-09-04 16:58:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config(object):
 	API_HASH = os.environ.get("API_HASH" "60d92ca6fcc5086d58444d6c24958efd")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL" "Assahai"))
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL" "-1001615276723"))
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
